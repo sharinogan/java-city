c468c406f535fdaa50db6c1d4f4d94e550a00ba4
Voragun <voragun2543@gmail.com>
2022-10-07 21:29:39 +0700

    flag method

--- code/Start.java
+++ code/Start.java
@@ -5,6 +5,14 @@ class Start {
         Scanner sc = new Scanner(System.in);
         
         String[] branches = new String[] { "New York", "London", "Paris", "Tokyo" };
-        
+        System.out.println("Enter City : ");
+        String s = sc.nextLine();
+        boolean flag = false;
+        for (int i = 0; i < branches.length; i++) {
+            if (branches[i].equals(s)) {
+                flag = true;
+            }
+        }
+        System.out.println(flag);
     }
 }
