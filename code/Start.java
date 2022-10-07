ca1d76c1713761ed2bfb2ded5b070c08d1e1db90
Voragun <voragun2543@gmail.com>
2022-10-07 21:18:13 +0700

    arrays new style

---
+++ code/Start.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Start {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String[] branches = new String[] { "New York", "London", "Paris", "Tokyo" };
+        
+    }
+}
